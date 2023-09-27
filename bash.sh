e56c15d69fc3f7cb8de16a28656d9240e6c27b64
Pavel Derzhuk <p.derzhuk@gmail.com>
2023-09-27 20:14:33 +0300

    add p.derzhuk update sh

--- bash.sh
+++ bash.sh
@@ -6,3 +6,5 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo touch /var/www/html/myweb.html
 sudo touch /var/www/html/myweb_test.html
+#### Hi branch devops ####
+echo "<h1>My test page by Pavlo Derzhuk<br>Deployed via Terraform</h1> <br><font color="green">Hello world ))!" | sudo tee /var/www/html/mytestweb.html
